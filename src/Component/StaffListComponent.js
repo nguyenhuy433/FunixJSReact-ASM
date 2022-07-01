8a604e758fb2f56e95a80d1955fa21b7e610c898
Nguyen Huy <nguyenhuy64220@gmail.com>
2022-07-01 15:32:47 +0700

    tim nhan vien

--- src/Component/StaffListComponent.js
+++ src/Component/StaffListComponent.js
@@ -19,9 +19,12 @@ class StaffList extends Component {
     super(pros);
     this.state = {
       isModalOpen: false,
+      findStaff: "",
     };
 
     this.toggleModal = this.toggleModal.bind(this);
+    this.handlefindStaff = this.handlefindStaff.bind(this);
+    this.FindStaff = this.FindStaff.bind(this);
   }
 
   // open modal them nhan vien
@@ -30,20 +33,45 @@ class StaffList extends Component {
       isModalOpen: !this.state.isModalOpen,
     });
   }
+  // tim kiem nhan vien
+
+  FindStaff() {
+    this.setState({
+      findStaff: this.findStaff.value,
+    });
+  }
+
+  handlefindStaff(event) {
+    this.FindStaff();
+    event.preventDefault();
+  }
 
   render() {
-    const staffList = this.props.staffs.map((staff) => {
-      return (
-        <div key={staff.id} className="col-md-4 col-lg-2 col-6 text-center">
-          <Card className="mt-4">
-            <Link to={`/staffs/${staff.id}`}>
-              <CardImg width="100%" src={staff.image} alt={staff.name} />
-              <CardTitle>{staff.name}</CardTitle>
-            </Link>
-          </Card>
-        </div>
-      );
-    });
+    const staffList = this.props.staffs
+      .filter((staff) => {
+        if (this.state.findStaff === "") {
+          return staff;
+        } else if (
+          staff.name.toLowerCase().includes(this.state.findStaff.toLowerCase())
+        ) {
+          return staff;
+        } else {
+          return 0;
+        }
+      })
+      .map((staff) => {
+        return (
+          <div key={staff.id} className="col-md-4 col-lg-2 col-6 text-center">
+            <Card className="mt-4">
+              <Link to={`/staffs/${staff.id}`}>
+                <CardImg width="100%" src={staff.image} alt={staff.name} />
+                <CardTitle>{staff.name}</CardTitle>
+              </Link>
+            </Card>
+          </div>
+        );
+      });
+
     console.log(staffList);
     return (
       <div className="container">
@@ -61,10 +89,15 @@ class StaffList extends Component {
             </div>
           </div>
           <div className="col-12 col-md-6 mt-4">
-            <Form className="form-search">
+            <Form className="form-search" onSubmit={this.handlefindStaff}>
               <FormGroup className="row">
                 <div className=" col-10 col-md-8">
-                  <Input type="text" className="form-control" />
+                  <Input
+                    type="text"
+                    className="form-control"
+                    placeholder="Tên nhân viên"
+                    innerRef={(input) => (this.findStaff = input)}
+                  />
                 </div>
                 <div className="col-2 col-md-4">
                   <Button className="button">Tìm</Button>
